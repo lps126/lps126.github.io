bfd063a4971e3d8134b44f9106eee1b949588556
richardj-lin <richardlin4375@gmail.com>
2025-11-26 13:27:24 -0500

    Mobile homepage done

---
+++ menu.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-button").onclick = function() {
+    document.getElementById("open-menu").style="right: 0px;";
+}
+
+document.getElementById("menu-close-button").onclick = function() {
+    document.getElementById("open-menu").style="right: -500px;";
+}
